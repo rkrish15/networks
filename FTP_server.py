3ff641f9b74c45569148836c1c25faa7a1d7a09d
Ravikrishna <119728678+rkrish15@users.noreply.github.com>
2023-11-08 22:43:11 +0530

    Add files via upload

---
+++ FTP_server.py
@@ -0,0 +1,15 @@
+import socket
+s = socket.socket()
+host = socket.gethostname() 
+port = 8080 
+s.bind((host,port)) 
+s.listen(1) 
+print(host)
+print("Waiting for any incoming connection... ")
+conn, addr = s.accept()
+print(addr, "Has connected to the server")
+filename = input(str("Enter the name of the file to be transmitted: "))
+file = open(filename , 'rb') 
+file_data = file.read(1024)
+conn.send(file_data)
+print("File has been transmitted successfully")
